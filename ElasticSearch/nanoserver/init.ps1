55244093e41b825e6cf6dbbec893d9ce6b214ad1
Tony Hernandez <9831944+TonyHernandezAtMS@users.noreply.github.com>
2018-02-21 22:23:14 -0600

    Working on volume points

--- ElasticSearch/nanoserver/init.ps1
+++ ElasticSearch/nanoserver/init.ps1
@@ -1,17 +1,17 @@
 if (-not (Test-Path g:\elasticsearch.yml)) {
     Write-Host "Missing config file. Using sample config file."
-    Copy-Item c:\elasticsearch\config-example\elasticsearch.yml g:\
+    Copy-Item c:\elasticsearch\config-example\elasticsearch.yml m:\
 }
 
 if (-not (Test-Path g:\jvm.options)) {
     Write-Host "Missing jvm.options file. Using sample jvm.options file."
-    Copy-Item c:\elasticsearch\config-example\jvm.options g:\
+    Copy-Item c:\elasticsearch\config-example\jvm.options m:\
 }
 
 if (-not (Test-Path g:\log4j2.properties)) {
     Write-Host "Missing log4j2.properties file. Using sample config file."
-    Copy-Item c:\elasticsearch\config-example\log4j2.properties g:\
+    Copy-Item c:\elasticsearch\config-example\log4j2.properties m:\
 }
 
 Write-Host "Starting ElasticSearch"
-C:\elasticsearch\bin\elasticsearch.exe -Epath.conf=g:\
+C:\elasticsearch\bin\elasticsearch.exe -Epath.conf=m:\ -Epath.logs=l:\ -Epath.data=g:\
