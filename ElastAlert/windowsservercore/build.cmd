f32c8a7638ee362021eece299e8592e6f93273e5
Tony Hernandez <9831944+TonyHernandezAtMS@users.noreply.github.com>
2018-02-26 16:43:32 -0800

    Updates for using ElastAlert with docker-compose

--- ElastAlert/windowsservercore/build.cmd
+++ ElastAlert/windowsservercore/build.cmd
@@ -1,2 +1,2 @@
-docker build -t tonyhernandezatms/elastalert:python-2.7.14-windowsservercore-ltsc2016 .
-REM docker push tonyhernandezatms/elastalert:python-2.7.14-windowsservercore-ltsc2016
+docker build -t tonyhernandezatms/elastalert:0.1.29_python-2.7.14_windowsservercore-ltsc2016 .
+docker push tonyhernandezatms/elastalert:0.1.29_python-2.7.14_windowsservercore-ltsc2016
