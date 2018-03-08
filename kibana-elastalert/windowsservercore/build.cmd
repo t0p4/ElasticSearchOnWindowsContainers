e31b5cbb627b5deadcac6117aab1b350b0c53661
Tony Hernandez <9831944+TonyHernandezAtMS@users.noreply.github.com>
2018-03-08 09:44:20 -0600

    Initial commit of kibana with EA plugin

---
+++ kibana-elastalert/windowsservercore/build.cmd
@@ -0,0 +1,2 @@
+docker build -t tonyhernandezatms/kibana-elastalert:windowsservercore-ltsc2016-ES_5.5.3-EA_0.1.2 .
+docker push tonyhernandezatms/kibana-elastalert:windowsservercore-ltsc2016-ES_5.5.3-EA_0.1.2
