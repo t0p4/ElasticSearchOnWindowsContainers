0aa44f7c6562da3672ac15c4f81f950cefbeaa6d
Tony Hernandez <9831944+TonyHernandezAtMS@users.noreply.github.com>
2018-03-08 12:46:26 -0600

    first bitsensor Dockerfile

--- bitsensor-elastalert/windowsservercore/build.cmd
+++ bitsensor-elastalert/windowsservercore/build.cmd
@@ -1,2 +1,2 @@
-docker build -t tonyhernandezatms/kibana-elastalert:windowsservercore-ltsc2016-ES_5.5.3-EA_latest .
-docker push tonyhernandezatms/kibana-elastalert:windowsservercore-ltsc2016-ES_5.5.3-EA_latest
+docker build -t tonyhernandezatms/bitsensor-elastalert:windowsservercore-ltsc2016-ES_5.5.3-EA_latest .
+docker push tonyhernandezatms/bitsensor-elastalert:windowsservercore-ltsc2016-ES_5.5.3-EA_latest
