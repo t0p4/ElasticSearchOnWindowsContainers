40aeb8d984165c8383a2cd6b9815e90dc4d912fb
top4 <top4skill@gmail.com>
2019-01-21 19:42:39 +0200

    Migrate to 1809

---
+++ build1809.cmd
@@ -0,0 +1,9 @@
+cd .\OpenJDK-powershell\nanoserver-1809
+start build.cmd
+cd ..\..\
+cd .\ElasticSearch\nanoserver-1809
+start build.cmd
+cd ..\..\
+cd .\Kibana\windowsservercore-1809
+start build.cmd
+cd ..\..\
